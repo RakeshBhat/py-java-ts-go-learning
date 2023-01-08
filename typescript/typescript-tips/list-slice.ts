d04156f35e50b692667b1fe5b56cd1a2b2db9f38
Rakesh Bhat <rakesh.bhatji@gmail.com>
2023-01-08 19:32:11 +0000

    Create list-slice.ts
    typescript list slice

---
+++ typescript/typescript-tips/list-slice.ts
@@ -0,0 +1,3 @@
+var fruits = ["apple", "orange", "mango", "banana", "sugar", "tea"];
+console.log("fruits.slice( 1, 2) : " + fruits.slice(1, 2));
+console.log("fruits.slice( 1, 3) : " + fruits.slice(1, 3));
